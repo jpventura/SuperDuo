f1c2eee5ba5c7acaa9cc9e094e708e38325b0c28
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-10-27 13:19:55 -0200

    Import Alexandria and Football Scores Android projects.
    Summary:
      - Add Apache 2.0 license.
      - Update to latest Android build environment.
      - Rename package names so them can be published on Google Play.
      - Add signing key configurations.

---
+++ Alexandria/app/src/main/java/com/jpventura/alexandria/api/BookListAdapter.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015 Udacity, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jpventura.alexandria.api;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jpventura.alexandria.R;
+import com.jpventura.alexandria.data.AlexandriaContract;
+import com.jpventura.alexandria.services.DownloadImage;
+
+/**
+ * Created by saj on 11/01/15.
+ */
+public class BookListAdapter extends CursorAdapter {
+
+
+    public static class ViewHolder {
+        public final ImageView bookCover;
+        public final TextView bookTitle;
+        public final TextView bookSubTitle;
+
+        public ViewHolder(View view) {
+            bookCover = (ImageView) view.findViewById(R.id.fullBookCover);
+            bookTitle = (TextView) view.findViewById(R.id.listBookTitle);
+            bookSubTitle = (TextView) view.findViewById(R.id.listBookSubTitle);
+        }
+    }
+
+    public BookListAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        String imgUrl = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
+        new DownloadImage(viewHolder.bookCover).execute(imgUrl);
+
+        String bookTitle = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
+        viewHolder.bookTitle.setText(bookTitle);
+
+        String bookSubTitle = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
+        viewHolder.bookSubTitle.setText(bookSubTitle);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.book_list_item, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+
+        return view;
+    }
+}
---
+++ Alexandria/app/src/main/java/com/jpventura/alexandria/data/AlexandriaContract.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2015 Udacity, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jpventura.alexandria.data;
+
+/**
+ * Created by saj on 22/12/14.
+ */
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+public class AlexandriaContract{
+
+    public static final String CONTENT_AUTHORITY = "com.jpventura.alexandria";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_BOOKS = "books";
+    public static final String PATH_AUTHORS = "authors";
+    public static final String PATH_CATEGORIES = "categories";
+
+    public static final String PATH_FULLBOOK = "fullbook";
+
+    public static final class BookEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_BOOKS).build();
+
+        public static final Uri FULL_CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_FULLBOOK).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        public static final String TABLE_NAME = "books";
+
+        public static final String TITLE = "title";
+
+        public static final String IMAGE_URL = "imgurl";
+
+        public static final String SUBTITLE = "subtitle";
+
+        public static final String DESC = "description";
+
+        public static Uri buildBookUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildFullBookUri(long id) {
+            return ContentUris.withAppendedId(FULL_CONTENT_URI, id);
+        }
+
+    }
+
+    public static final class AuthorEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_AUTHORS).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_AUTHORS;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_AUTHORS;
+
+        public static final String TABLE_NAME = "authors";
+
+        public static final String AUTHOR = "author";
+
+        public static Uri buildAuthorUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class CategoryEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_CATEGORIES).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_CATEGORIES;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_CATEGORIES;
+
+        public static final String TABLE_NAME = "categories";
+
+        public static final String CATEGORY = "category";
+
+        public static Uri buildCategoryUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+    }
+}
