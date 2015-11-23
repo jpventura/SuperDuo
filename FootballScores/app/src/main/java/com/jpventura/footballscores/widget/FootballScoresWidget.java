5f6009c1d8544d9ad28d50aa5ebe694288551390
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-11-22 22:36:03 -0200

    create widget

---
+++ FootballScores/app/src/main/java/com/jpventura/footballscores/widget/FootballScoresWidget.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Udacity, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jpventura.footballscores.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.jpventura.footballscores.R;
+import com.jpventura.footballscores.app.MainActivity;
+
+public class FootballScoresWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+//        for (int id : appWidgetIds) {
+//            String packageName = context.getPackageName();
+//            RemoteViews remoteViews = new RemoteViews(packageName, R.layout.widget_football_scores);
+//
+//            Intent intent = new Intent(context, MainActivity.class);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+//            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+//
+//            views.setRemoteAdapter(R.id.scores_list, new Intent(context, ScoresWidgetRemoteViewsService.class));
+//            views.setEmptyView(R.id.scores_list, R.id.scores_empty);
+//
+//            appWidgetManager.updateAppWidget(appWidgetId, views);
+//        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
